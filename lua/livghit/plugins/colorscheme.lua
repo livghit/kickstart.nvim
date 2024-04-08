aa68b37909d1e7a75b13e40034c5af8ee7b4873a
livghit <liviu.g01@gmail.com>
2024-04-08 21:42:08 +0200

    wip it

--- lua/livghit/plugins/colorscheme.lua
+++ lua/livghit/plugins/colorscheme.lua
@@ -1,17 +1,19 @@
 return {
+
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
   --'folke/tokyonight.nvim', -- Tokyonight colorscheme
-  'nyoom-engineering/oxocarbon.nvim', -- Oxocarbon colorscheme
+  --'nyoom-engineering/oxocarbon.nvim', -- Oxocarbon colorscheme
   --  'rose-pine/neovim', -- Rosepine neovim
   -- name = 'rose-pine',
-
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000, -- make sure to load this before all the other start plugins
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.opt.background = 'dark' -- set this to dark or light
-    vim.cmd.colorscheme 'oxocarbon'
+    vim.cmd.colorscheme 'catppuccin'
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
   end,
