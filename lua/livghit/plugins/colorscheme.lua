8937b630e1cafc4787f62d181047bcb721b1a3cf
livghit <liviu.g01@gmail.com>
2024-09-09 07:51:25 +0200

    ⛏️ wip

--- lua/livghit/plugins/colorscheme.lua
+++ lua/livghit/plugins/colorscheme.lua
@@ -15,7 +15,7 @@ return {
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.opt.background = 'dark' -- set this to dark or light
-    vim.cmd.colorscheme 'tokyonight'
+    vim.cmd.colorscheme 'tokyonight-storm'
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
   end,
